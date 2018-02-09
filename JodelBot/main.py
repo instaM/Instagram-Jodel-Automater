528d0efca13784defd6f72c96811b7314959a799
Gregor Ziegltrum <gregi11@web.de>
2018-02-09 23:35:40 +0100

    Fix

--- JodelBot/main.py
+++ JodelBot/main.py
@@ -1,6 +1,6 @@
 from JodelBot import JodelBot
 jbot = JodelBot()
-jbot.scanTopPost(400)
-jbot.getBestImage(True).show()
+#jbot.scanTopPost(400)
+jbot.getBestImage().show()
 #print(jbot.getTopPost("Bremen"))
 #jbot.scanTopPost(0)
