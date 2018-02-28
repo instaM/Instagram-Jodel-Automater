e3c5417cb917bdf690581fdcaa66b391da4159f5
Gregor Ziegltrum <gregi11@web.de>
2018-02-28 18:03:02 +0100

    Added bot entity

--- apis/JodelBot/JodelPost.py
+++ apis/JodelBot/JodelPost.py
@@ -2,6 +2,7 @@ import  Post
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+import os
 class JodelPost(Post):
 
     def __init__(self,text,city,votes,color):
@@ -10,16 +11,23 @@ class JodelPost(Post):
         self.votes = votes
         self.color = color
     def getImage(self):
+        city = self.city
+        text = self.text
+        votes = self.votes
         crgb = tuple(int(self.color[i:i + 2], 16) for i in (0, 2, 4))
         back = Image.new("RGB", (640,640), crgb)
-        front = Image.open("elements.png")
+        front = Image.open( os.path.abspath(os.path.join(__file__, '..' ,"elements.png")))
         back.paste(front,(0,0),front)
         draw = ImageDraw.Draw(back)
-        font = ImageFont.truetype("gbr.otf", 28)
-        fontVotes = ImageFont.truetype("gbr.otf", 28)
-        fontCity = ImageFont.truetype("gbr.otf", 20)
+        font = ImageFont.truetype(os.path.abspath(os.path.join(__file__, '..' ,"gbr.otf")), 28)
+        fontVotes = ImageFont.truetype(os.path.abspath(os.path.join(__file__, '..' ,"gbr.otf")), 28)
+        fontCity = ImageFont.truetype(os.path.abspath(os.path.join(__file__, '..' ,"gbr.otf")), 20)
+        city.encode('iso-8859-1')
+        city = city.replace("ue",u"ü")
+        city = city.replace("ae", u"ä")
+        city = city.replace("oe", u"ö")
         splitLength = 28
-        text = self.text.replace("\n\n", "\n")
+        text = text.replace("\n\n", "\n")
         textArray = text.split(" ")
         for i in range(0,len(textArray)):
             if len(textArray[i]) > splitLength:
@@ -44,9 +52,10 @@ class JodelPost(Post):
         offset = len(newArray) * -16
         for i in range(0, len(newArray)): #printe alle zeilen auf den screen
             draw.text((40, 277 + offset + 32 * i), newArray[i], (255, 255, 255), font=font)
-        offset = len(str(self.votes)) * -7
-        draw.text((575 + offset, 273), str(self.votes), (255, 255, 255), font=fontVotes)
-        draw.text((65, 600), self.city, (255, 255, 255), font=fontCity)
+        offset = len(str(votes)) * -7
+        draw.text((575 + offset, 273), str(votes), (255, 255, 255), font=fontVotes)
+        draw.text((65, 600), city, (255, 255, 255), font=fontCity)
+
         return back
     def getHashtags(self):
         #hashtag 1 = self.city
