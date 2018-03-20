a3137af020edc696dfe5d4e9fd32e7fb57237472
root <root@raspberrypi>
2018-03-20 12:17:05 +0000

    Updated badkeyword

--- instabot/src/instabot.py
+++ instabot/src/instabot.py
@@ -109,7 +109,7 @@ class Instabot:
         return tag_list_left
         
     def containsBadKeyWord(self,name):
-      name =
+      
       for bad in self.blacklist_usertags:
         
         if bad in str(name).lower(): 
